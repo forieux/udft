1d6775b2b665067eb9936b7f74e47126ae141919
François Orieux <orieux@l2s.centralesupelec.fr>
2021-04-24 21:31:10 +0200

    Bump version: 3.1.1 → 3.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ author = "François Orieux"
 # The short X.Y version
 version = "3.0"
 # The full version, including alpha/beta/rc tags
-release = "3.1.1"
+release = "3.1.2"
 
 
 # -- General configuration ---------------------------------------------------
