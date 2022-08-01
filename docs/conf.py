805c80a688b7c80afcd34ab081eaf29702895744
François Orieux <orieux@l2s.centralesupelec.fr>
2022-08-01 10:15:04 +0200

    Bump version: 3.4.0 → 3.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ author = "François Orieux"
 # The short X.Y version
 version = "3.0"
 # The full version, including alpha/beta/rc tags
-release = "3.4.0"
+release = "3.5.0"
 
 
 # -- General configuration ---------------------------------------------------
--- udft/__init__.py
+++ udft/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "3.4.0"
+__version__ = "3.5.0"
 version_info = (3, 4, 0)
 
 from .udft import *
